339b822af671896b51e43d2198d4a3745b5ea7f7
kkek <komaleshwarakumarkonatham@gmail.com>
2023-01-29 11:31:39 +0530

    test2

--- Main.java
+++ Main.java
@@ -3,16 +3,17 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.*;
 
-import packages.read;
-public class Main {
+import packages.*;
+public class Main extends write {
   public static Scanner inputs;
   public static void main(String[] args) throws IOException
   {
-    if(read.inputs == null){
+    if(inputs == null){
       System.out.println(read.readinput());
     }
-    while (read.inputs != null && read.inputs.hasNextLine()) {
+    while (inputs != null && read.inputs.hasNextLine()) {
       System.out.println(read.readinput());
     }
+    output();
   }
 }
---
+++ packages/write.java
@@ -0,0 +1,17 @@
+package packages;
+
+import java.util.function.Function;
+
+public class write extends read {
+     public static void output() {
+        
+        System.out.print("output will written now");
+        
+
+        
+
+
+
+
+    }
+}
